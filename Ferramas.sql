ff38179cac4a2da291e769b7824d6faf7d6d3d4d
BigDarkness3232 <fre.cardenas@duocuc.cl>
2024-05-10 12:41:50 -0400

    algunos cambios
    algunos errores

--- Ferramas.sql
+++ Ferramas.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `Feramas`
+-- Base de datos: `Ferramas`
 --
 CREATE DATABASE IF NOT EXISTS `Ferramas` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
 USE `Ferramas`;
